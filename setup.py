d8e5607dedaf0ff73172c4e9b34fa68de5d15a86
NMurdaugh <nmurdaugh42@gmail.com>
2023-01-31 13:17:13 -0700

    merged code from fy 2.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-<<<<<<< HEAD
 VERSION = '2.0.0'
-=======
-VERSION = '1.1.1'
->>>>>>> 7a04fa1269750b2bf0b83ad7937c0f75e26a3ee4
 DESCRIPTION = 'Enhanced FAKEYOU.COM API lib'
 LONG_DESCRIPTION = 'None'
 
@@ -25,8 +21,9 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['requests','aiohttp'],
-    keywords=['fakeyou','fakeyou.py','fakeyou api','rappers voice','sing AI','text to speech','text to singing','text to song','lyrics to song','tts'],
+    install_requires=['requests', 'aiohttp'],
+    keywords=['fakeyou', 'fakeyou.py', 'fakeyou api', 'rappers voice', 'sing AI',
+              'text to speech', 'text to singing', 'text to song', 'lyrics to song', 'tts'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
